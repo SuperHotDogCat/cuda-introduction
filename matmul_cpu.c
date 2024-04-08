59e7594b7b63d3b33ac3332139754a19d0902ea6
SuperHotDogCat <siromisochan@gmail.com>
2024-04-09 02:01:30 +0900

    Add implement: measure time

--- matmul_cpu.c
+++ matmul_cpu.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void init_matrix(double *mat, double init_num, int dim){
     // mat is expected to be a 2-dimentional matrix expressed by a 1-dimentional array. 
@@ -23,10 +24,15 @@ void matmul_cpu(double *input_mat1, double *input_mat2, double *output_mat, int 
     }
 }
 
+double calculate_elapsed_time(struct timespec start_time, struct timespec end_time){
+    return (double) (end_time.tv_sec - start_time.tv_sec) + (double) (end_time.tv_nsec - start_time.tv_nsec) * 1e-9;
+}
+
 int main(int argc, char **argv){
     int n;
     // add error processing here
     n = atoi(argv[1]);
+    struct timespec start_time, end_time;
     double *input_mat1 = (double *)malloc(sizeof(double)*n*n);
     double *input_mat2 = (double *)malloc(sizeof(double)*n*n);
     double *output_mat = (double *)malloc(sizeof(double)*n*n);
@@ -34,6 +40,8 @@ int main(int argc, char **argv){
     init_matrix(input_mat2, 0.1, n);
     init_matrix(output_mat, 0.0, n);
     // start to measure time
+    clock_gettime(CLOCK_REALTIME, &start_time);
     matmul_cpu(input_mat1, input_mat2, output_mat, n);
-    printf("ok\n");
+    clock_gettime(CLOCK_REALTIME, &end_time);
+    printf("elapsed time %f\n", calculate_elapsed_time(start_time, end_time));
 }
